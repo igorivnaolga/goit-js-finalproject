3295c714782e1fd664c9d5f980b0518d0b644e0d
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2023-12-22 04:44:22 +0200

    modal window, basiclightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,6 +4,7 @@ import { galleryItems } from './gallery-items.js';
 const container = document.querySelector('.gallery');
 
 container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+container.addEventListener('click', handlerImgClick);
 
 function createMarkup(arr) {
   return arr
@@ -23,3 +24,35 @@ function createMarkup(arr) {
     )
     .join('');
 }
+
+function handlerImgClick(evt) {
+  if (evt.currentTarget === evt.target) {
+    return;
+  }
+  evt.preventDefault();
+  const instance = basicLightbox.create(
+    `<div class="modal">
+        <img src="${evt.target.dataset.source}" alt="${evt.target.alt}"/>
+          
+    </div>`,
+    {
+      handler: null,
+      onShow(instance) {
+        this.handler = onEscape.bind(instance);
+        document.addEventListener('keydown', this.handler);
+      },
+      onClose() {
+        document.removeEventListener('keydown', this.handler);
+      },
+    }
+  );
+  instance.show();
+}
+
+function onEscape({ code }) {
+  if (code === 'Escape') {
+    this.close();
+  }
+}
+
+console.log(galleryItems);
